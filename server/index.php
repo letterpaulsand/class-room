6992a736a5cd168151b96d3cfdee6a824e0e5d38
letterpaulsand <paul097958@gmail.com>
2022-01-24 00:22:18 +0800

    php creat ok

--- server/index.php
+++ server/index.php
@@ -1,7 +1,10 @@
 <?php
+/*
+*       MADE BY PAUL
+*/
 header('Access-Control-Allow-Origin:*');
 $sqlhost = 'localhost';
-$user ='root';
+$user = 'root';
 $password = 'mouse0609';
 $dbname = 'class';
 // get GET start
@@ -16,21 +19,38 @@ $month = $_GET['month'];
 $date = $_GET['date'];
 $day = $_GET['day'];
 // get GET end
-$table = $year.$month.$date;
+$table = $year . $month . $date;
 
 // connect sql
-$connent = mysqli_connect($sqlhost,$user,$password,$dbname);
-if($connent){
-    $creattable = "CREATE TABLE  $table (sendlist TEXT(4),sendjob TEXT(4),sendbook TEXT(10),jobdate TEXT(20),page TEXT(30),des TEXT(100),year TEXT(10),month TEXT(2),date TEXT(2),day TEXT(1))";
-    if(mysqli_query($link,$creattable)){
-        echo 'ok';
-    }else{
-        echo 'error';
-    };
-}else {
+$connent = mysqli_connect($sqlhost, $user, $password, $dbname);
+if ($connent) {
+    $creattable = "CREATE TABLE `$table` (
+        sendlist TEXT(4),
+        sendjob TEXT(4),
+        sendbook TEXT(10),
+        jobdate TEXT(20),
+        pages TEXT(30),
+        dess TEXT(100),
+        years TEXT(10),
+        months TEXT(2),
+        dates TEXT(2),
+        dayss TEXT(1)
+        )";
+
+
+if ($connent->query($creattable) === TRUE) {
+    echo "ok";
+   
+} else {
+    echo "error creat";
+}
+
+
+
+} else {
+
     echo "database error :" . mysqli_connect_error();
 };
 
 
-echo $sendlist, $sendjob, $sendbook, $jobdate, $page, $des, $year, $month, $date, $day;
-?>
+// echo $sendlist, $sendjob, $sendbook, $jobdate, $page, $des, $year, $month, $date, $day;
