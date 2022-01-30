988c8bae08fe6cf22c6c91c019a4196b6975e366
letterpaulsand <letterpaulsand@gmail.com>
2022-01-31 01:18:14 +0800

    all ok

--- src/html/index/index.js
+++ src/html/index/index.js
@@ -2,9 +2,17 @@ import "./index.css";
 import "../../js/alert.js";
 const editor = document.getElementById('editor');
 const look = document.getElementById('look');
+const setting = document.getElementById('setting');
+const documents = document.getElementById('document') 
 look.addEventListener('click', ()=>{
     location.href = '../look'
 })
 editor.addEventListener('click', ()=>{
     location.href = '../main'
+})
+setting.addEventListener('click', ()=>{
+    location.href = '../setting'
+})
+documents.addEventListener('click', ()=>{
+    location.href = '../document'
 })
